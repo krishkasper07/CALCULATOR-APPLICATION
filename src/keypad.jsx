105a91827f72968efb3f694ddc7f08dad42c2a19
kishore <kishore.9980602683@gmail.com>
2021-06-02 13:02:43 +0530

    keypad with map()

--- src/keypad.jsx
+++ src/keypad.jsx
@@ -1,25 +1,9 @@
 import Button from './Button.jsx';
 
 function Keypad(){
-    return (<div className="keypad">
-        <Button value="7"/>
-        <Button value="8"/>
-        <Button value="9"/>
-        <Button value="&divide;"/>
-        <Button value="4"/>
-        <Button value="5"/>
-        <Button value="6"/>
-        <Button value="*"/>
-        <Button value="1"/>
-        <Button value="2"/>
-        <Button value="3"/>
-        <Button value="-"/>
-        <Button value="0"/>
-        <Button value="."/>
-        <Button value="+"/>
-        <Button value="="/>
-        </div>
-    );
+    return <div className="keypad">
+    {["7",'8','9','/','4','5','6','*','1','2','3','-','0','.','+','='].map(el=> <Button value={el}/>)}
+          </div>
 }
 
 export default Keypad;
